6e86e37e8797fc820e48a505187716b21d4cb54b
Oleh Semenenko <o.semenenko.mar@gmail.com>
2023-04-24 12:43:14 +0300

    added 2 methods

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,87 @@
+<!-- First method -->
+
 <template>
+  <div class="max-w-[1440px] p-6">
+    <h3 class="font-medium m-0 inline-block">Contact list</h3>
+    <button
+      type="button"
+      class="ml-5 hover:text-blue-500"
+      @click="createEmptyForm"
+    >
+      Add contact
+    </button>
+
+    <div class="contact-list grid-cols-[repeat(auto-fill,_minmax(320px,_1fr))] grid gap-5 my-5">
+      <ContactItem
+        v-for="(contact, index) in contacts"
+        :key="contact.id"
+        :contact="contact"
+        @delete="deleteContact(index)"
+        @save="onContactSave($event, index)"
+        @create="createNewCard($event, index)"
+        @cancel="cancelForm"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { IContact } from '@/types'
+import ContactItem from '@/components/ContactItem.vue'
+
+const contacts = ref<IContact[]>([
+  {
+    id: 1,
+    name: 'Esther Howard',
+    description: 'Forward Response Developer',
+    image: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+  },
+  {
+    id: 2,
+    name: 'Jane Cooper',
+    description: 'Regional Paradigm Technician Regional Paradigm Technician Regional Paradigm Technician',
+    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+  },
+  {
+    id: 3,
+    name: 'Cody Fisher',
+    description: 'Product Directives Officer',
+    image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
+  }
+])
+
+function deleteContact (index: number) {
+  contacts.value.splice(index, 1)
+}
+
+function onContactSave (contact: IContact, index: number) {
+  contacts.value[index] = { ...contact }
+}
+
+function createNewCard (newData: IContact, index: number) {
+  contacts.value[index] = { ...newData, id: Date.now() }
+}
+
+function createEmptyForm () {
+  const newData = {
+    id: 0,
+    name: '',
+    description: '',
+    image: ''
+  }
+
+  contacts.value.unshift(newData)
+}
+
+function cancelForm () {
+  contacts.value.splice(0, 1)
+}
+</script>
+
+<!-- Second method -->
+
+<!-- <template>
   <div class="max-w-[1440px] p-6">
     <h3 class="font-medium m-0 inline-block">Contact list</h3>
     <button
@@ -35,7 +118,6 @@
 import { ref } from 'vue'
 import type { IContact } from '@/types'
 import ContactItem from '@/components/ContactItem.vue'
-// import NewEmptyCard from '@/components/NewEmptyCard.vue'
 
 const contacts = ref<IContact[]>([
   {
@@ -72,4 +154,4 @@ function createNewCard (newData: IContact) {
   contacts.value.unshift(newData)
   isEmptyForm.value = false
 }
-</script>
+</script> -->
--- src/components/ContactItem.vue
+++ src/components/ContactItem.vue
@@ -1,6 +1,8 @@
+<!-- First method -->
+
 <template>
-  <div class="rounded-lg bg-white shadow relative">
-    <div class="p-6 pb-2">
+  <div class="rounded-lg bg-white shadow relative flex flex-col">
+    <div class="p-6 pb-2 flex-grow">
       <div class="flex">
         <div class="flex-grow text-sm truncate">
           <template v-if="editMode || isNewCard">
@@ -9,19 +11,182 @@
               v-model="localContact.name"
               type="text"
               class="block text-gray w-full border border-blue-500 rounded-lg px-2 py-1"
-              placeholder="name"
+              placeholder="Enter your name"
             >
             <input
               v-model="localContact.description"
               type="text"
               class="block mt-1 text-gray w-full border border-blue-500 rounded-lg px-2 py-1"
-              placeholder="description"
+              placeholder="Enter description"
             >
             <input
               v-model="localContact.image"
               type="text"
               class="block mt-1 text-gray w-full border border-blue-500 rounded-lg px-2 py-1"
-              placeholder="image"
+              placeholder="Enter image url"
+            >
+          </template>
+
+          <template v-else>
+            <p class="font-medium">{{ contact.name }}</p>
+            <p class="text-gray mt-1 truncate">
+              {{ contact.description }}
+            </p>
+          </template>
+        </div>
+        <img
+          v-if="!isNewCard"
+          class="w-[40px] h-[40px] object-cover ml-2 rounded-full shrink-0"
+          :src="contact.image" alt="contact-logo"
+        >
+      </div>
+      <div class="flex justify-end mt-2 gap-2">
+        <template v-if="editMode || isNewCard">
+          <span
+            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
+            @click="isNewCard ? cancelForm() : editMode = false"
+          >Cancel</span>
+
+          <span
+            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
+            :class="{'opacity-60 cursor-not-allowed': isBtnDisabled }"
+            @click="isNewCard ? createCard() : onSave()"
+          >Save</span>
+        </template>
+
+        <template v-else>
+          <span
+            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
+            @click="triggerEditMode"
+          >Edit</span>
+
+          <span
+            class="text-red-500 font-medium text-xs cursor-pointer hover:underline"
+            @click="$emit('delete')"
+          >Delete</span>
+        </template>
+      </div>
+    </div>
+
+    <div v-if="!isNewCard" class="flex text-sm font-medium text-gray-dark border-t border-gray-ultra-light">
+      <div class="flex items-center justify-center flex-1 py-4 cursor-pointer hover:text-gray">
+        <IconEnvelope />
+        <span class="ml-3">Email</span>
+      </div>
+      <div
+        class="flex items-center justify-center flex-1 py-4 border-l
+            border-gray-ultra-light cursor-pointer hover:text-gray"
+      >
+        <IconPhone />
+        <span class="ml-3">Call</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, nextTick, watchEffect, onMounted } from 'vue'
+import type { IContact } from '@/types'
+import IconEnvelope from '@/components/icons/IconEnvelope.vue'
+import IconPhone from '@/components/icons/IconPhone.vue'
+
+const props = defineProps<{
+  contact: IContact
+}>()
+
+const emit = defineEmits(['delete', 'save', 'create', 'cancel'])
+
+const inputRef = ref<HTMLInputElement>()
+
+const localContact = ref<Omit<IContact, 'id'>>({
+  name: '',
+  description: '',
+  image: ''
+})
+
+const editMode = ref(false)
+
+async function triggerEditMode () {
+  editMode.value = true
+  localContact.value = { ...props.contact }
+  await nextTick()
+  inputRef.value?.focus()
+
+  const { name, description } = localContact.value
+  if (!name || !description) {
+    isBtnDisabled.value = true
+  }
+}
+
+function onSave () {
+  createDefaultAvatar()
+  if (!isBtnDisabled.value) {
+    emit('save', localContact.value)
+    editMode.value = false
+  }
+}
+
+const isBtnDisabled = ref(false)
+
+const isNewCard = ref(false)
+
+function createCard () {
+  createDefaultAvatar()
+  if (!isBtnDisabled.value) {
+    emit('create', localContact.value)
+    editMode.value = false
+    isNewCard.value = false
+  }
+}
+
+function cancelForm () {
+  emit('cancel')
+}
+
+function createDefaultAvatar () {
+  if (!localContact.value.image) {
+    localContact.value.image = 'https://static.vecteezy.com/system/resources/previews/021/548/095/original/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg'
+  }
+}
+
+watchEffect(() => {
+  isBtnDisabled.value = !localContact.value.name || !localContact.value.description
+})
+
+onMounted(() => {
+  if (props.contact.id === 0) {
+    isNewCard.value = true
+  }
+})
+
+</script>
+
+<!-- Second method -->
+
+<!-- <template>
+  <div class="rounded-lg bg-white shadow relative flex flex-col">
+    <div class="p-6 pb-2 flex-grow">
+      <div class="flex">
+        <div class="flex-grow text-sm truncate">
+          <template v-if="editMode || isNewCard">
+            <input
+              ref="inputRef"
+              v-model="localContact.name"
+              type="text"
+              class="block text-gray w-full border border-blue-500 rounded-lg px-2 py-1"
+              placeholder="Enter your name"
+            >
+            <input
+              v-model="localContact.description"
+              type="text"
+              class="block mt-1 text-gray w-full border border-blue-500 rounded-lg px-2 py-1"
+              placeholder="Enter description"
+            >
+            <input
+              v-model="localContact.image"
+              type="text"
+              class="block mt-1 text-gray w-full border border-blue-500 rounded-lg px-2 py-1"
+              placeholder="Enter image url"
             >
           </template>
 
@@ -111,13 +276,13 @@ async function triggerEditMode () {
   await nextTick()
   inputRef.value?.focus()
   const { name, description } = localContact.value
-
   if (!name || !description) {
     isBtnDisabled.value = true
   }
 }
 
 function onSave () {
+  createDefaultAvatar()
   if (!isBtnDisabled.value) {
     emit('save', localContact.value)
     editMode.value = false
@@ -127,6 +292,7 @@ function onSave () {
 const isBtnDisabled = ref(false)
 
 function createCard () {
+  createDefaultAvatar()
   if (!isBtnDisabled.value) {
     emit('create', localContact.value)
     editMode.value = false
@@ -137,8 +303,13 @@ function cancelForm () {
   emit('cancel')
 }
 
+function createDefaultAvatar () {
+  if (!localContact.value.image) {
+    localContact.value.image = 'https://static.vecteezy.com/system/resources/previews/021/548/095/original/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg'
+  }
+}
+
 watchEffect(() => {
   isBtnDisabled.value = !localContact.value.name || !localContact.value.description
 })
-
-</script>
+</script> -->
